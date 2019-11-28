6e3540cfdf7ad65d2da4ca830c71206fe91e5336
ruvinda <ruvinda@pickme.lk>
2019-11-28 11:41:53 +0530

    this is the tesing branch comit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+	public function index() //this is for git testing 
 	{
 		$this->load->view('welcome_message');
 	}
